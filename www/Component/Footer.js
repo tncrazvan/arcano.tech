57306636abc9ff6bea6b8d81c883ce00c5de126a
Razvan Tanase <tangent.jotey@gmail.com>
2019-11-09 19:40:12 +0100

    Update

---
+++ www/Component/Footer.js
@@ -0,0 +1,8 @@
+Components.$init("/Footer",function(){
+    this.appendChild(create("small","Developed by Razvan Tanase"));
+    this.appendChild(create("small","All rights reserved 2019"));
+    this.css({
+        display: "grid",
+        justifyItems: "center"
+    });
+});
