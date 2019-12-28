9165f5ea901892c8eb0882baf8d9cfdaea4271b1
Razvan <tangent.jotey@gmail.com>
2019-12-28 16:42:06 +0100

    updated for Arcano 1.1.0

--- server/src/main/java/com/github/tncrazvan/arcanotech/Starter.java
+++ server/src/main/java/com/github/tncrazvan/arcanotech/Starter.java
@@ -1,12 +1,12 @@
 package com.github.tncrazvan.arcanotech;
 
-import com.github.tncrazvan.arcano.Server;
+import com.github.tncrazvan.arcano.Arcano;
 import com.github.tncrazvan.arcanotech.controller.App;
 import com.github.tncrazvan.arcanotech.controller.Documentation;
 
 public class Starter{
     public static void main(final String[] args) {
-        Server server = new Server(
+        new Arcano(
             args,
             App.class,
             Documentation.class
--- www/metadata/Starter.java
+++ www/metadata/Starter.java
@@ -1,11 +1,11 @@
 package $namespace;
 
-import com.github.tncrazvan.arcano.ArcanoServer;
+import com.github.tncrazvan.arcano.Arcano;
 import $namespace.Controller.HelloWorld;
 
 public class Starter{
     public static void main(final String[] args) {
-        new ArcanoServer(
+        new Arcano(
             args,
             HelloWorld.class
         );
