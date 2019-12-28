61e739ae580d7922ba6dfd7df23bfb83d025dfc7
Razvan <tangent.jotey@gmail.com>
2019-12-28 15:33:33 +0100

    Starter.java changed

--- www/metadata/Starter.java
+++ www/metadata/Starter.java
@@ -1,11 +1,11 @@
 package $namespace;
 
-import com.github.tncrazvan.arcano.Server;
+import com.github.tncrazvan.arcano.ArcanoServer;
 import $namespace.Controller.HelloWorld;
 
 public class Starter{
     public static void main(final String[] args) {
-        Server server = new Server(
+        new ArcanoServer(
             args,
             HelloWorld.class
         );
