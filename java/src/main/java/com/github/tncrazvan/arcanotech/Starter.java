5ac3a73b8b75f335a576cc60f241bb30b596044d
Razvan <tangent.jotey@gmail.com>
2020-02-16 23:54:46 +0100

    Update

--- java/src/main/java/com/github/tncrazvan/arcanotech/Starter.java
+++ java/src/main/java/com/github/tncrazvan/arcanotech/Starter.java
@@ -4,13 +4,18 @@ import com.github.tncrazvan.arcano.Arcano;
 import com.github.tncrazvan.arcano.Http.HttpController;
 import com.github.tncrazvan.arcanotech.controller.App;
 import com.github.tncrazvan.arcanotech.controller.Documentation;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
 
 public class Starter extends HttpController{
-    public static void main(final String[] args) {
+    
+    public static void main(final String[] args) throws IOException, NoSuchAlgorithmException, ClassNotFoundException, URISyntaxException {
         new Arcano(
             args,
             App.class,
-            Documentation.class
+            Documentation.class,
+            Starter.class
         );
     }
 }
