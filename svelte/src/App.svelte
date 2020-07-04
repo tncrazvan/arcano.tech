f736f3832eba9a1cd6bb3c420a53b008147fdf1c
Razvan <tangent.jotey@gmail.com>
2020-07-04 17:19:06 +0200

    Update App.svelte

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,11 +1,11 @@
 <script>
+	import OtherFramework7Styles from './../shared/components/styles/OtherFramework7Styles.svelte';
+	import GridStyles from './../shared/components/styles/GridStyles.svelte';
+	import PaddingStyles from './../shared/components/styles/PaddingStyles.svelte';
+	import MarginStyles from './../shared/components/styles/MarginStyles.svelte';
+	import OtherStyles from './../shared/components/styles/OtherStyles.svelte';
 	import TopNav from './components/TopNav.svelte';
 	import CreatePage from './pages/CreatePage.svelte';
-	import OtherFramework7Styles from './shared/components/styles/OtherFramework7Styles.svelte';
-	import GridStyles from './shared/components/styles/GridStyles.svelte';
-	import PaddingStyles from './shared/components/styles/PaddingStyles.svelte';
-	import MarginStyles from './shared/components/styles/MarginStyles.svelte';
-	import OtherStyles from './shared/components/styles/OtherStyles.svelte';
   import HomePage from './pages/HomePage.svelte';
   import {Router,Route,navigate} from 'svelte-routing';
   import {
